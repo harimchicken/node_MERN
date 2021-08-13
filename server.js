fad10456124476e7ac7974c78cf697b7dc60fda0
leeharim <idegami1559@icloud.com>
2021-08-13 19:15:51 +0900

    add route

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ const app = express();
 
 require('./config/database')
 
+
+// routing
+const usersRoute = require('./routes/users')
+const profilesRoute = require('./routes/profile')
+const postsRoute = require('./routes/posts')
+
+
 // middleware
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
+
+
+// Use Routes
+app.use('/api/users', usersRoute)
+app.use('/api/profile', profilesRoute)
+app.use('/api/posts', postsRoute)
 
 
 const PORT = process.env.PORT || 3000;
