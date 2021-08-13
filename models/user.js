ed85204cf9e52cd2687bb5caaec7bd6daf9143f6
leeharim <idegami1559@icloud.com>
2021-08-13 20:31:31 +0900

    add code refactoring

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+const avatar = require('gravatar')
+const normalize = require('normalize-url')
+const gravatar = require('gravatar/lib/gravatar')
 
 // 회원가입시 필요했던 내용
 const userSchema = new Schema(
@@ -24,6 +28,37 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+// 저장하기 전에 아바타하고 패스워드를 암호화 해서 저장 기능을 만듬
+userSchema.pre('save', async function (next) {
+    try {
+        console.log('entered')
+        // 아바타 생성
+        const avatar = await normalize(
+            gravatar.url(this.email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            }),
+            { forceHttps: true }
+        )
+        
+        this.avatar = avatar;
+        // 패스워드 암호화
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+
+        this.password = hashedPassword
+
+        console.log('exited')
+        next()
+
+
+    } catch (error) {
+        next(error)
+    }
+}) 
+
 
 
 module.exports = user = mongoose.model('user', userSchema)
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,5 @@
 
 const express = require('express')
-const bcrypt = require('bcryptjs')
-const gravatar = require('gravatar')
-const normalize = require('normalize-url')
 const router = express.Router()
 
 const userModel = require('../models/user')
@@ -25,71 +22,27 @@ router.post('/register', (req, res) => {
                 })
             } else {
 
-                // 어떤것을 기준으로 만들거냐
-                const avatar =  normalize(
-                    gravatar.url(email, {
-                        s: '200',   // size
-                        r: 'pg',    // Rating
-                        d: 'mm'     // Default
-                    }),
-                    { forceHttps: true}
-                )
-
-                // 패스워드 암호화
-                bcrypt.hash(password, 10, (err, hash) => {
-                    if (err) throw err;
-                    // 디비에 저장
-                    const newUser = new userModel({
-                        name, email, password: hash, avatar
-                    })
-                    newUser
-                        .save()
-                        .then(user => res.json(user))
-                        .catch(err => res.status(408).json(err))
+                const newUser = new userModel({
+                    name, email, password
                 })
+
+                newUser
+                    .save()
+                    .then(user => {
+                        user.password = undefined;
+                        res.json({
+                            message: "successful signup",
+                            userInfo: user
+                        })
+                    })
+                    .catch(err => {
+                        res.status(500).json(err)
+                    })
+
             }
         })
         .catch(err => res.status(500).json(err))
 
-
-
-    // userModel
-    //     .findOne({email: req.body.email})
-    //     .then(user => {
-    //         if (user) {
-    //             return res.status(400).json({
-    //                 err: "기존에 등록된 이메일이 있습니다, 다른 이메일로 가입 바랍니다."
-    //             })
-    //         } else {
-    //             bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    //                 if(err) {
-    //                     return res.status(404).json({
-    //                         error: err
-    //                     })
-    //                 } else {
-    //                     const newUser = new userModel({
-    //                         user: req.body.user,
-    //                         email: req.body.email,
-    //                         password: hashedPassword
-    //                     })
-            
-    //                     newUser
-    //                         .save()
-    //                         .then(user => {
-    //                             res.json({
-    //                                 msg: "successful signup",
-    //                                 userInfo: user
-    //                             })
-    //                         })
-    //                         .catch(err => {
-    //                             res.status(500).json({
-    //                                 err: err.message
-    //                             })
-    //                         })
-    //                 }
-    //             })
-    //         }
-    //     })
 })
 
 
