3d767afa86173a5da7f5c6860521f45784a3d977
leeharim <idegami1559@icloud.com>
2021-08-19 20:37:55 +0900

    add profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,31 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('passport')
 
+const checkauth = passport.authenticate('jwt', { session: false});
+
+const profileModel = require('../models/profile')
+
+
+// @route   GET api/profile 
+// @desc    GET current users profile
+// @access  Private
+// api 다음에 profile은 url 접근시 localhost:7000/api/profile
+
+router.get('/', checkauth, (req, res) => {
+    profileModel
+        .findOne( {user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                return res.status(404).json({
+                    message: 'There is no profile for this user'
+                })
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(500).json(err))
+})
 
 
 module.exports = router
