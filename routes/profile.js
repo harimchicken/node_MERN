e451ffc3806de8f4e1f48770c53d375a3557fcd0
leeharim <idegami1559@icloud.com>
2021-08-24 19:25:34 +0900

    add profile update #3

--- routes/profile.js
+++ routes/profile.js
@@ -54,23 +54,19 @@ router.post('/', checkauth, (req, res) => {
         .findOne( {user: req.user.id })
         .then(profile => {
             if (profile) {
-                // update code
-                profileModel
-                    .findOneAndUpdate(
-                        {user: req.user.id}, // 업데이트 할 대상자
-                        { $set: profileFields}, // 업데이트 내용
-                        { new: true}  // 옵션
-                    )
-                    .then(profile => res.json(profile))
-                    .catch(err => res.status(408).json(err))
+                // // update code
+                // profileModel
+                //     .findOneAndUpdate(
+                //         {user: req.user.id}, // 업데이트 할 대상자
+                //         { $set: profileFields}, // 업데이트 내용
+                //         { new: true}  // 옵션
+                //     )
+                //     .then(profile => res.json(profile))
+                //     .catch(err => res.status(408).json(err))
 
-
-
-
-
-                // return res.status(400).json({
-                //     msg: "등록된 프로필이 있습니다. 업데이트 또는 삭제 후 다시 등록해주세요"
-                // })
+                return res.status(400).json({
+                    msg: "등록된 프로필이 있습니다. 업데이트 또는 삭제 후 다시 등록해주세요"
+                })
             } else {
                 const newProfile = new profileModel(profileFields)
                 newProfile
@@ -83,5 +79,51 @@ router.post('/', checkauth, (req, res) => {
 
 
 })
+
+// @route   Put api/profile 
+// @desc    udadate user profile
+// @access  Private
+
+router.put('/', checkauth, (req, res) => {
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.user) profileFields.user = req.user.id
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubusename) profileFields.githubusename = req.body.githubusename
+
+    // Skills - Split into array
+    if (typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',');
+    }
+
+    profileModel
+        .findOne( {user: req.user.id} )
+        .then(profile => {
+            if (!profile) {
+                res.status(404).json({
+                    msg: "please register your profile"
+                })
+            } else {
+                // update code
+                profileModel
+                    .findOneAndUpdate(
+                        {user: req.user.id}, // 업데이트 할 대상자
+                        { $set: profileFields}, // 업데이트 내용
+                        { new: true}  // 옵션
+                    )
+                    .then(profile => res.json(profile))
+                    .catch(err => res.status(408).json(err))
+            }
+        })
+        .catch(err => res.status(500).json(err))
+}) 
+
+
 
 module.exports = router
