540b080d341b51c7044235b9e47093b9710ea011
leeharim <idegami1559@icloud.com>
2021-08-29 18:20:36 +0900

    add post model

--- config/database.js
+++ config/database.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose');
 mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
+        useUnifiedTopology: true
     })
     .then(() => console.log('MongoDB Connected ...'))
     .catch(err => console.log(err))
---
+++ models/post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const{Schema,Model} = mongoose;
+
+// Create Schema
+const PostSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        likes: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'user'
+                }
+            }
+        ],
+        comment: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'user'
+                },
+                text: {
+                    type: String,
+                    required: true
+                },
+                name: {
+                    type: String
+                },
+                date: {
+                    type: Date,
+                    default: Date.now
+                }
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+module.exports  = model('post', PostSchema)
